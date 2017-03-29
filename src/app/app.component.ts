fefbb6c17479aa14926242ce62d3ef0ffd0a0d67
Guarani1 <jpgarritano@cespi.unlp.edu.ar>
2017-03-29 14:30:47 -0300

    Menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { Platform, MenuController, Nav } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { TabsPage } from '../pages/tabs/tabs';
+import { MapaRondinPage } from "../pages/mapa-rondin/mapa-rondin";
+
+
 
 
 @Component({
@@ -11,13 +14,30 @@ import { TabsPage } from '../pages/tabs/tabs';
 })
 export class MyApp {
   rootPage = TabsPage;
-
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  @ViewChild(Nav) nav: Nav;
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, private menu: MenuController) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
+      
     });
+
+    
+  }
+
+  openMenu() {
+      this.menu.open();
+    }
+
+    openPageMapaRondin() {
+    // the nav component was found using @ViewChild(Nav)
+    // reset the nav to remove previous pages and only have this page
+    // we wouldn't want the back button to show in this scenario
+    this.nav.setRoot(MapaRondinPage).catch(() => {
+        console.log("Didn't set nav root");
+      });
+    
   }
 }
