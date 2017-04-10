ace06e6945bea386f944369012aeac4468c8e5da
Luciano Coggiola <tanoinc@gmail.com>
2017-04-10 10:25:49 -0300

    Clase provider para llamados al webservice

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,8 @@ import { TabsPage } from '../pages/tabs/tabs';
 import { MapaRondinPage } from '../pages/mapa-rondin/mapa-rondin';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { HttpModule } from '@angular/http';
+
 
 @NgModule({
   declarations: [
@@ -19,6 +21,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     MapaRondinPage
   ],
   imports: [
+    HttpModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
